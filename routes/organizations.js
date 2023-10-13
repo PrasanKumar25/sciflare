ccfebf2dd394a1fc954f65b96dbe5d9f8c545494
PrasanKumar25 <94027469+PrasanKumar25@users.noreply.github.com>
2023-10-13 17:32:18 +0530

    Add files via upload

---
+++ routes/organizations.js
@@ -0,0 +1,9 @@
+
+const express = require("express");
+const router = express.Router();
+const Organization = require("../models/organization");
+const passport = require("../middleware/passport");
+
+
+
+module.exports = router;
