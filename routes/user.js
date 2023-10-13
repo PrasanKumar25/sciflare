a228e93a2e3f73487ff8ae8c8222a15676378df9
PrasanKumar25 <94027469+PrasanKumar25@users.noreply.github.com>
2023-10-13 17:36:44 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,10 @@
 
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const passport = require("../middleware/passport");
+
+
+
+module.exports = router;
+
