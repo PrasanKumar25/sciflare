86d7070db80faa7110dac69ac45199d049d08955
PrasanKumar25 <94027469+PrasanKumar25@users.noreply.github.com>
2023-10-13 17:36:06 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  role: String, 
+  organization: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Organization",
+  },
+  
+});
+
+module.exports = mongoose.model("User", userSchema);
+
